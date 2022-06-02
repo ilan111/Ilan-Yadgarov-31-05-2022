33c3535ae860a13ced7aaafd6664de531a384ef8
Ilan Y <ilanydg@gmail.com>
2022-06-02 16:50:45 +0300

    Updated navigation bar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
-
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.css'
 import Navigation from "./Components/Navbar/Navigation";
 import FavoritesPage from "./Pages/Favorites page/FavoritesPage";
 import MainPage from "./Pages/Main page/MainPage";
+import Background from "./images/bg.jpg";
 
 function App() {
   return (
     <div className="App">
+      <img className="App__bg" src={Background}></img>
       <Navigation />
-      <FavoritesPage />
-      <MainPage />
+      {/* <FavoritesPage /> */}
+      {/* <MainPage /> */}
     </div>
   );
 }
--- src/Components/Navbar/Navigation.js
+++ src/Components/Navbar/Navigation.js
@@ -2,18 +2,35 @@ import React from "react";
 import "./Navigation.css";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import Container from 'react-bootstrap/Container'
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MainPage from "../../Pages/Main page/MainPage";
+import FavoritesPage from "../../Pages/Favorites page/FavoritesPage";
 
 function Navigation() {
   return (
-    <Nav variant="tabs" defaultActiveKey="/main">
-      <Nav.Item>
-        <Nav.Link href="/main">Main</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="favourites">Favourites</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">Weather App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/favorites">
+                Favorties
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </Router>
   );
 }
 
